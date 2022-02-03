a198f988e5c8ccd0b3a088f25a31f10f6313814c
Jessica <jesvadtz@gmail.com>
2022-02-02 21:39:58 -0600

    Agregando paginación

--- functions.js
+++ functions.js
@@ -1,36 +1,68 @@
+let page = 1;
+let printTable;
+
 $(document).ready(function () {
-  let consapi = {
-    url: `https://api.coingecko.com/api/v3/coins/markets?vs_currency=mxn`,
-    method: "GET",
-    timeout: 0,
-  };
+  printTable = (page) => {
+    let consapi = {
+      url: `https://api.coingecko.com/api/v3/coins/markets?vs_currency=mxn&page=${page}`,
+      method: "GET",
+      timeout: 0,
+    };
 
-  $.ajax(consapi).done(function (response) {
-    // console.log(response);
-    let dataInfo = response;
+    $.ajax(consapi).done(function (response) {
+      // console.log(response);
+      let dataInfo = response;
 
-    dataInfo.forEach((object) => {
-      let imagesCripto = object.image;
+      dataInfo.forEach((object) => {
+        let imagesCripto = object.image;
 
-      $("#dataCripto").append(
-        $("<tr/>").append(
-          $("<th/>")
-            .attr("class", "image")
-            .append(
-              $("<img/>").attr({
-                src: `${imagesCripto}`,
-              })
-            ),
-          $("<th/>").attr("class", "name").text(object.id),
-          $("<th/>").attr("class", "simbolo").text(object.symbol),
-          $("<th/>").attr("class", "precio").text(`$ ${object.current_price}`),
-          $("<th/>").attr("class", "precioAlto").text(`$ ${object.high_24h}`),
-          $("<th/>").attr("class", "precioAlto").text(`$ ${object.low_24h}`),
-          $("<th/>")
-            .attr("class", "precioAlto")
-            .text(new Date(object.ath_date).toLocaleString())
-        )
-      );
+        $("#dataCripto").append(
+          $("<tr/>").append(
+            $("<th/>")
+              .attr("class", "image")
+              .append(
+                $("<img/>").attr({
+                  src: `${imagesCripto}`,
+                })
+              ),
+            $("<th/>").attr("class", "name").text(object.id),
+            $("<th/>").attr("class", "simbolo").text(object.symbol),
+            $("<th/>")
+              .attr("class", "precio")
+              .text(`$ ${object.current_price}`),
+            $("<th/>").attr("class", "precioAlto").text(`$ ${object.high_24h}`),
+            $("<th/>").attr("class", "precioAlto").text(`$ ${object.low_24h}`),
+            $("<th/>")
+              .attr("class", "precioAlto")
+              .text(new Date(object.ath_date).toLocaleString())
+          )
+        );
+      });
     });
-  });
+  };
+  printTable();
 });
+
+//Pagination
+$("#pages").append("<div/>"), $("div").attr("id", "pagination");
+
+for (let i = 1; i < 31; i++) {
+  let botonPag = document.createElement("button");
+  botonPag.innerText = i;
+
+  // console.log(botonPag);
+
+  botonPag.onclick = () => {
+    page = parseInt(i);
+    deleteAllinDiv();
+    printTable(i);
+  };
+  $("#pagination").append(botonPag);
+}
+
+//Función para borrar elementos
+function deleteAllinDiv() {
+  while (dataCripto.hasChildNodes()) {
+    dataCripto.removeChild(dataCripto.lastChild);
+  }
+}
